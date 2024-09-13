dad041d18d7af1bfcdfdeccdd053fb1be4f266d2
marcelboo <1038695@hr.nl>
2024-09-13 19:26:25 +0200

    comments to the providers

--- CargoHub/api/providers/auth_provider.py
+++ CargoHub/api/providers/auth_provider.py
@@ -1,3 +1,5 @@
+#Depending on the user that's logged in "Admin" or "Client" you get these boolean values
+#These values either allow you as admin to use REST API request or only the GET request.
 USERS = [
     {
         "api_key": "a1b2c3d4e5",
@@ -94,16 +96,20 @@ USERS = [
 
 _users = None
 
+#The user gets initialized
 def init():
     global _users
     _users = USERS
 
+#Search for a user based on their API key. When the key is not found it returns none
 def get_user(api_key):
     for x in _users:
         if x["api_key"] == api_key:
             return x
     return None
 
+#Checks if the user has acces to API request. If the user has "full" access they are allowed to perform every operation.
+#Else it checks for if the user has access to said API request eg. (GET)
 def has_access(user, path, method):
     access = user["endpoint_access"]
     if access["full"]:
